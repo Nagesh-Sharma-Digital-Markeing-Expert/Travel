8cedcf287e273c557db39cb6b1f74dadadcfd240
Nagesh Sharma <nageshsharma760@gmail.com>
2024-06-20 22:30:02 +0530

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,38 @@
+<?php 
+define('CNF_APPNAME','');
+define('CNF_APPDESC','');
+define('CNF_COMNAME','Tam Tour & Travels');
+define('CNF_ADDRESS','abcd');
+define('CNF_TEL','123456789');
+define('CNF_EMAIL','varun7king@gmail.com');
+define('CNF_FACEBOOK','https://www.facebook.com/');
+define('CNF_TWITTER','https://www.twitter.com/');
+define('CNF_INSTAGRAM','https://www.instagram.com/');
+define('CNF_TRIPADVISOR','https://www.tammanager.com/');
+define('CNF_TAGLINE','Plan your Ride');
+define('CNF_TEMPCOLOR','red-dark');
+define('CNF_METAKEY','');
+define('CNF_METADESC','');
+define('CNF_GROUP','6');
+define('CNF_ACTIVATION','confirmation');
+define('CNF_MAINTENANCE','');
+define('CNF_SHOWHELP','ON');
+define('CNF_MULTILANG','1');
+define('CNF_LANG','en');
+define('CNF_REGIST','true');
+define('CNF_FRONT','true');
+define('CNF_RECAPTCHA','false');
+define('CNF_THEME','leo');
+define('CNF_RECAPTCHAPUBLICKEY','');
+define('CNF_RECAPTCHAPRIVATEKEY','');
+define('CNF_MODE','production');
+define('CNF_LOGO','logo.jpg');
+define('CNF_HEADERIMAGE','header.jpg');
+define('CNF_ALLOWIP','');
+define('CNF_RESTRICIP','');
+define('CNF_MAIL','phpmail');
+define('CNF_DATE','M d Y');
+define('CNF_APIKEY','');
+define('CNF_CALENDARID','');
+define('CNF_ANALYTICS','UA-XXXXX-X');
+?>
